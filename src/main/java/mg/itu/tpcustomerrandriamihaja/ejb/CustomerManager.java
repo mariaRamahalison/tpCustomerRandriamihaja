26e6e2c5797c219334547ac497e6787589969358
mariaRamahalison <mariaramahalison@gmail.com>
2023-03-18 21:35:43 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/itu/tpcustomerrandriamihaja/ejb/CustomerManager.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpcustomerrandriamihaja.ejb;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+import mg.itu.tpcustomerrandriamihaja.entities.Customer;
+
+/**
+ *
+ * @author Maria
+ */
+@Stateless
+public class CustomerManager {
+    
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    } 
+
+    
+    
+}
